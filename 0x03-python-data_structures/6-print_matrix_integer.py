95e66bd0b2207c87460db6d992b20099d368b7ca
Julija Lee <lee.julija@gmail.com>
2017-05-10 04:59:32 +0000

    added check if matrix exists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    numrows = len(matrix)
-    numcols = len(matrix[0])
-    for i in range(0, numrows):
-        for j in range(0, numcols):
-            print('{:d}'.format(matrix[i][j]), end=' ')
-        print()
+    if matrix:
+        numrows = len(matrix)
+        numcols = len(matrix[0])
+        for i in range(0, numrows):
+            for j in range(0, numcols):
+                print('{:d}'.format(matrix[i][j]), end=' ')
+            print()
