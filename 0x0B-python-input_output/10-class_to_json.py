efef98990029b21a2fca482e7bc3899fced0200b
Julija Lee <lee.julija@gmail.com>
2017-06-04 05:03:37 +0000

    fixed comments

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""module: 10-class_to_json"""
+
+
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure (list,"""
-    """dictionary, string, integer and boolean) for JSON serialization of"""
-    """an object"""
+    """class_to_json"""
     return obj.__dict__
