a765d9326e5453c9038c2ca992726f5cee1a3e1a
Julija Lee <lee.julija@gmail.com>
2017-07-13 07:06:49 +0000

    fixed a typo

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- script that displays the average temperature by city ordered by temperature (descending)
-SELECT city, AVG(values) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
