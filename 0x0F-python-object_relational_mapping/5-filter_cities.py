7b1f633316d152df649fe693b13873150774bf22
Julija Lee <lee.julija@gmail.com>
2017-07-26 07:03:01 +0000

    changed string

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.name "
-                "FROM cities "
-                "WHERE cities.state_id = "
-                "(SELECT states.id "
-                "FROM states "
-                "WHERE states.name = '{}') "
-                "ORDER BY cities.id ASC".format(search_state))
+    cur.execute("SELECT cities.name \
+                FROM cities \
+                WHERE cities.state_id = \
+                (SELECT states.id \
+                FROM states \
+                WHERE states.name = '{}') \
+                ORDER BY cities.id ASC".format(search_state))
     query_rows = cur.fetchall()
     for row in query_rows:
         new_list.append(row[0])
