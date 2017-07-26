f9f0850fe2944201e9b5787e9ee693245e507f01
Julija Lee <lee.julija@gmail.com>
2017-07-26 06:07:44 +0000

    changed triple quote to single

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """
 4-cities_by_state.py - cities from the database hbtn_0e_4_usa
 """
@@ -10,12 +9,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("""
-                SELECT cities.id, cities.name, states.name
-                FROM cities JOIN states
-                ON cities.state_id = states.id
-                ORDER BY cities.id ASC
-                """)
+    cur.execute("SELECT cities.id, cities.name, states.name"
+                "FROM cities JOIN states"
+                "ON cities.state_id = states.id"
+                "ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
