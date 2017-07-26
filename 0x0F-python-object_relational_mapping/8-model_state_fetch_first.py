0ee07bb44b187abdd5833a2eb97de6d470d3c3e2
Julija Lee <lee.julija@gmail.com>
2017-07-26 09:39:47 +0000

    forgot else Nothing

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,5 +16,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id)[0:1]:
-        print("{}: {}".format(state.id, state.name))
+        if state:
+            print("{}: {}".format(state.id, state.name))
+        else:
+            print("Nothing")
     session.close()
