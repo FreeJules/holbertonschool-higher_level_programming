b3bb4ddb001a9e5a28594b1471a8c3648c243337
Julija Lee <lee.julija@gmail.com>
2017-07-26 07:07:38 +0000

    adding 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+2-my_filter_states.py - displays all values where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    search_for = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".
+                format(search_for))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
