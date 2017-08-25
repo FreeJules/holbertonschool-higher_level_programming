70f6ddd46e4b41323d89ac48989d09ace8fd6a18
Julija Lee <lee.julija@gmail.com>
2017-08-25 09:30:13 +0000

    adding 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# manages urllib.error.HTTPError exceptions
+import urllib.request
+import sys
+
+url = sys.argv[1]
+req = urllib.request.Request(url)
+try:
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print (html.decode('utf8'))
+except urllib.error.HTTPError as e:
+    print ('Error code: {}'.format(e.code))
