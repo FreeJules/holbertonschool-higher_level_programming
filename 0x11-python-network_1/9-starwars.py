042926c93c1eb147004bac8c4b9d8be03ebd6ed9
Julija Lee <lee.julija@gmail.com>
2017-08-25 17:12:38 +0000

    updtaed modify to results

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     value = {'search': sys.argv[1]}
     r = requests.get(url, params=value)
     json = r.json()
-    print('Number of result: {}'.format(json.get("count")))
+    print('Number of results: {}'.format(json.get("count")))
     for i in json.get("results"):
         print(i.get("name"))
