bd8127b2a5c279e4f6b405d8a18072170fa0de80
Julija Lee <lee.julija@gmail.com>
2017-08-25 13:54:47 +0000

    moved args to main

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,11 +6,11 @@ import requests
 import sys
 from requests.auth import HTTPBasicAuth
 
-url = 'https://api.github.com/user'
-user_name = sys.argv[1]
-passwd = sys.argv[2]
 
 if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
     r = requests.get(url, auth=HTTPBasicAuth(user_name, passwd))
     json = r.json()
     print('{}'.format(json.get("id")))
