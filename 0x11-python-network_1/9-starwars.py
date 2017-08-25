633a62cb67f614f833a7f79c920793b010946acd
Julija Lee <lee.julija@gmail.com>
2017-08-25 13:56:05 +0000

    moved vars inside main

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -4,12 +4,13 @@
 """
 import requests
 import sys
-url = 'https://swapi.co/api/people/'
+
 
 if __name__ == "__main__":
+    url = 'https://swapi.co/api/people/'
     value = {'search': sys.argv[1]}
     r = requests.get(url, params=value)
     json = r.json()
     print('Number of result: {}'.format(json.get("count")))
     for i in json.get("results"):
-        print (i.get("name"))
+        print(i.get("name"))
