11817d4c74cb7c9f3411f81d63a7e1f51ac22d63
Julija Lee <lee.julija@gmail.com>
2017-09-23 09:58:27 +0000

    adding 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that display the status code of a GET request
+var request = require('request');
+let requestURL = process.argv[2];
+request(requestURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
