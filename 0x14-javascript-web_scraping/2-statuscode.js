0dc5b950c6f00d80ed78ad551009065b68c16d8b
Julija Lee <lee.julija@gmail.com>
2017-09-23 10:00:25 +0000

    used var but not allowed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that display the status code of a GET request
-var request = require('request');
+let request = require('request');
 let requestURL = process.argv[2];
 request(requestURL, function (error, response, body) {
   if (error) {
