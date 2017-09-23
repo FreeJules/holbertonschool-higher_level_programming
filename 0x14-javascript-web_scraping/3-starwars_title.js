411e87cb228548295962522c444ce4b4373a82a2
Julija Lee <lee.julija@gmail.com>
2017-09-23 10:12:29 +0000

    adding 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that prints the title of a Star Wars movie where the episode number matches a given integer
+var request = require('request');
+let episodeNumber = process.argv[2];
+let requestURL = 'http://swapi.co/api/films/' + episodeNumber;
+request(requestURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let bodyJson = JSON.parse(body);
+    console.log(bodyJson['title']);
+  }
+});
