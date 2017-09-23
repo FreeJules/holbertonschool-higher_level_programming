1adef213547c3c55b3edba0150619d294ef2ceb4
Julija Lee <lee.julija@gmail.com>
2017-09-23 10:32:25 +0000

    switched full url for just 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints the number of movies where the character "Wedge Antilles" is present
 let request = require('request');
-let character = 'https://swapi.co/api/people/18/';
+let character = '18';
 let requestURL = process.argv[2];
 request(requestURL, function (error, response, body) {
   if (error) {
@@ -11,8 +11,10 @@ request(requestURL, function (error, response, body) {
     let listOfFilms = bodyJson.results;
     let count = 0;
     for (let i = 0; i < listOfFilms.length; i++) {
-      if (listOfFilms[i].characters.includes(character)) {
-        count++;
+      for (let j = 0; j < listOfFilms[i].characters.length; j++) {
+        if (listOfFilms[i].characters[j].includes(character)) {
+          count++;
+        }
       }
     }
     console.log(count);
