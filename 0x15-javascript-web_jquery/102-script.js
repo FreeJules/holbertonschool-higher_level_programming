ac3f00e7d2f28998a06fee0b675e2becf8e1cefd
Julija Lee <lee.julija@gmail.com>
2017-09-28 04:28:54 +0000

    adding 102

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('INPUT#btn_search').click(function () {
+    // Get
+    let city = $('INPUT#city_search').val();
+    // Set
+    $('INPUT#city_search').val(city);
+    $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:city_name%22)&format=json', function (data) {
+      $('DIV#wind_speed').append(data.query.results.channel.wind.speed);
+    });
+  });
+});
