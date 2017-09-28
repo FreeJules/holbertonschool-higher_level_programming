523630e0e2c0c6557794c051dede85bcd5be1fad
Julija Lee <lee.julija@gmail.com>
2017-09-28 04:34:30 +0000

    forgot to update city name in url

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     let city = $('INPUT#city_search').val();
     // Set
     $('INPUT#city_search').val(city);
-    $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:city_name%22)&format=json', function (data) {
+    $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:' + city + '%22)&format=json', function (data) {
       $('DIV#wind_speed').append(data.query.results.channel.wind.speed);
     });
   });
